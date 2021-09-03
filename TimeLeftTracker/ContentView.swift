d5c2f5bed4ccc16172d16efda4e3015161753e89
Anton Einarson <anton.einarson@gmail.com>
2021-09-04 00:32:46 +0200

    Initial Commit

---
+++ TimeLeftTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLeftTracker
+//
+//  Created by AppTeam on 2021-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeLeftTracker/TimeLeftTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLeftTrackerApp.swift
+//  TimeLeftTracker
+//
+//  Created by AppTeam on 2021-09-04.
+//
+
+import SwiftUI
+
+@main
+struct TimeLeftTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
